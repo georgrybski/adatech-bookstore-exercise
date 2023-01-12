1b861a07d20aa467e35bb9f49a2e59875abc3665
georgrybski <rybskigeorg@gmail.com>
2023-01-12 12:40:03 -0300

    Implemented Toy Class' main properties and methods

--- src/br/com/georg/library/products/Toy.java
+++ src/br/com/georg/library/products/Toy.java
@@ -1,4 +1,34 @@
 package br.com.georg.library.products;
 
-public class Toy {
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class Toy extends Product {
+    private String type;
+    private static Map<Integer,String> types = new HashMap(Map.of());
+//    private static Stream<Map.Entry<Integer,String>> typesStream = types.entrySet().stream();
+    private static String[] typeMenuOptions =
+        types.entrySet()
+                .stream()
+                .map(entry -> entry.getKey() + " - " + entry.getValue())
+                .toArray(String[]::new);
+
+    public Toy(String name, BigDecimal price, Integer quantity, Integer type){
+        super(name, price, quantity);
+        this.type = types.get(type);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public static Map<Integer, String> getTypes() {
+        return types;
+    }
+
+    public static String[] getTypeMenuOptions() {
+        return typeMenuOptions;
+    }
 }
