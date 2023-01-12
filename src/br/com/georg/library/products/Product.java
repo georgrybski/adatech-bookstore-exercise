4f0c34894c24273088aa56c1e087c5256df73220
georgrybski <rybskigeorg@gmail.com>
2023-01-12 12:14:58 -0300

    Added Product Class properties and methods

--- src/br/com/georg/library/products/Product.java
+++ src/br/com/georg/library/products/Product.java
@@ -1,4 +1,42 @@
 package br.com.georg.library.products;
 
+import java.math.BigDecimal;
+
 public class Product {
+    private String name;
+    private Integer ID;
+    private BigDecimal price;
+    private Integer quantity;
+    private static Integer IDCounter = 1;
+
+    public Product(String name, BigDecimal price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        ID = IDCounter++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
