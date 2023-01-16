81a0a24d8913a14c680104f7318cc9fd3c1a2087
georgrybski <rybskigeorg@gmail.com>
2023-01-16 16:35:40 -0300

    Added category specific product printing; Optimized imports

--- src/br/com/georg/bookstore/utilities/Printer.java
+++ src/br/com/georg/bookstore/utilities/Printer.java
@@ -1,6 +1,6 @@
 package br.com.georg.bookstore.utilities;
 
-import br.com.georg.bookstore.products.Product;
+import br.com.georg.bookstore.products.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -192,10 +192,30 @@ public class Printer {
         printFrameLine();
     }
 
-    public static void printProduct(Product product, String genre, String category) {
+    public static void printProduct(Product product, String genreType, String category) {
         printFrameLine();
-        printIndentedFormattedLine(genre + " " + category.substring(0, category.length() - 1)+ " | ID: " + product.getID());
+        printIndentedFormattedLine(genreType + " " + category.substring(0, category.length() - 1)+ " | ID: " + product.getID());
         printIndentedFormattedLine(product.getName() + " | Price: " + product.getPrice() + " USD");
+
+        switch (category) {
+            case "Albums" -> {
+                Album album = (Album) product;
+                printFormattedMultiLineString("Author: " + album.getAuthor() + " | Record label: " + album.getRecordLabel());
+            }
+            case "Books" -> {
+                Book book = (Book) product;
+                printFormattedMultiLineString("Author: " + book.getAuthor() + " | Published by: " + book.getPublisher());
+            }
+            case "Films" -> {
+                Film film = (Film) product;
+                printFormattedMultiLineString("Directed by: " + film.getDirectors() + " | Produced by: " + film.getProducer());
+            }
+            case "Games" -> {
+                Game game = (Game) product;
+                printFormattedMultiLineString("Studio: " + game.getStudio() + " | Distribuitor: " + game.getDistribuitor());
+            }
+        }
+
         printIndentedFormattedLine(product.getQuantity() > 0 ? "In Stock - Only " + product.getQuantity() + " Left" : "Out of Stock");
         printFrameLine();
     }
