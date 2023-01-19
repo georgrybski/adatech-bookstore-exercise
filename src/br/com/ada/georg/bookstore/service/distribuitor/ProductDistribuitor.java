cbf7b4e0d36047a43f6d26a4b4a5edf940acbd9b
georgrybski <rybskigeorg@gmail.com>
2023-01-19 19:29:49 -0300

    Moved toyData from ProductDistribuitor to ToyData

--- src/br/com/ada/georg/bookstore/service/distribuitor/ProductDistribuitor.java
+++ src/br/com/ada/georg/bookstore/service/distribuitor/ProductDistribuitor.java
@@ -61,50 +61,6 @@ public class ProductDistribuitor {
         }
     }
     public static class ProductList {
-
-        public static Object[][] toyData = new Object[][] {
-//              {String name, BigDecimal price, int quantity, String genre}
-
-                {"Toys", "Lego Porche 911 RSR 42096", BigDecimal.valueOf(169.99), "Action figure", 100},
-                {"Toys", "Lego Porche 911 RSR 42096", BigDecimal.valueOf(169.99), "Action figure", 100},
-
-                {"Toys", "Lego Porche 911 RSR 42096", BigDecimal.valueOf(169.99), "Animal", 100},
-                {"Toys", "Lego Porche 911 RSR 42096", BigDecimal.valueOf(169.99), "Animal", 100},
-
-                {"Toys", "Lego Porche 911 RSR 42096", BigDecimal.valueOf(169.99), "Car and radio controlled", 100},
-                {"Toys", "Lego Porche 911 RSR 42096", BigDecimal.valueOf(169.99), "Car and radio controlled", 100},
-
-                {"Toys", "Lego Porche 911 RSR 42096", BigDecimal.valueOf(169.99), "Construction", 100},
-                {"Toys", "Lego Porche 911 RSR 42096", BigDecimal.valueOf(169.99), "Construction", 100},
-
-                {"Toys", "Lego Porche 911 RSR 42096", BigDecimal.valueOf(169.99), "Creative", 100},
-                {"Toys", "Lego Porche 911 RSR 42096", BigDecimal.valueOf(169.99), "Creative", 100},
-
-                {"Toys", "Lego Porche 911 RSR 42096", BigDecimal.valueOf(169.99), "Doll", 100},
-                {"Toys", "Lego Porche 911 RSR 42096", BigDecimal.valueOf(169.99), "Doll", 100},
-
-                {"Toys", "Lego Porche 911 RSR 42096", BigDecimal.valueOf(169.99), "Educational", 100},
-                {"Toys", "Lego Porche 911 RSR 42096", BigDecimal.valueOf(169.99), "Educational", 100},
-
-                {"Toys", "Lego Porche 911 RSR - 42096", BigDecimal.valueOf(169.99), "Electronic", 100},
-                {"Toys", "Lego Hogwarts Castle - 71043 ", BigDecimal.valueOf(469.99), "Electronic", 100},
-
-                {"Toys", "Perpetual Motion Desktop Gadget", BigDecimal.valueOf(35.36), "Executive", 100},
-                {"Toys", "Golden Winged Fidget Spinner", BigDecimal.valueOf(57.99), "Executive", 100},
-
-                {"Toys", "Theo Klein Miele Petit Gourmet Play Kitchen", BigDecimal.valueOf(89.99), "Food-related", 100},
-                {"Toys", "So Slime DIY Slimelicious Factory", BigDecimal.valueOf(31.99), "Food-related", 100},
-
-                {"Toys", "CubicFun Titanic 3D", BigDecimal.valueOf(36.99), "Puzzle", 100},
-                {"Toys", "Starry Night Vincent Van Gogh Jigsaw - 5070cm", BigDecimal.valueOf(12.99), "Puzzle", 100},
-
-                {"Toys", "KidzLab Anti-Gravity Maglev", BigDecimal.valueOf(43.99), "Science and optical", 100},
-                {"Toys", "NATIONAL GEOGRAPHIC Break Open 10 Premium Geodes", BigDecimal.valueOf(34.99), "Science and optical", 100},
-
-                {"Toys", "Baby Einstein Take Along Tunes", BigDecimal.valueOf(9.99), "Sound and music", 100},
-                {"Toys", "Hape Mighty Echo Microphone", BigDecimal.valueOf(11.99), "Sound and music", 100}
-        };
-
         public static Object[][][] productData = new Object[][][] {
                 albumData, bookData, filmData, gameData, toyData
         };
